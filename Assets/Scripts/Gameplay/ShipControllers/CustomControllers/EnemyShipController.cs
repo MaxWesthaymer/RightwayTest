bf63ad61f4799f23a18e27357257f05b49e3988f
thehdcorp@gmail.com <thehdcorp@gmail.com>
2022-03-24 00:49:36 +0900

    Snake move enemy

--- Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
+++ Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using Gameplay.ShipControllers;
 using Gameplay.ShipSystems;
 using UnityEngine;
---
+++ Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemySnakeShipController.cs
@@ -0,0 +1,37 @@
+﻿using Gameplay.ShipSystems;
+using Gameplay.Spaceships;
+using UnityEngine;
+
+namespace Gameplay.ShipControllers.CustomControllers
+{
+    public class EnemySnakeShipController : EnemyShipController
+    {
+        [SerializeField]private float _changeDirectionCooldown = 0.5f;
+        private int _lateralDirection;
+        private float _cooldown;
+
+        public override void Init(ISpaceship spaceship)
+        {
+            base.Init(spaceship);
+            _lateralDirection = 1;
+            _cooldown = _changeDirectionCooldown;
+        }
+
+        protected override void ProcessHandling(MovementSystem movementSystem)
+        {
+            movementSystem.LongitudinalMovement(Time.deltaTime);
+            SnakeMovement(movementSystem);
+        }
+
+        private void SnakeMovement(MovementSystem movementSystem)
+        {
+            if (_cooldown <= 0)
+            {
+                _cooldown = _changeDirectionCooldown;
+                _lateralDirection *= -1;
+            }
+            movementSystem.LateralMovement(Time.deltaTime * _lateralDirection);
+            _cooldown -= Time.deltaTime;
+        }
+    }
+}
